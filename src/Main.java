66de857d1aa7b47ed9b89de8ba1e1308a9ab956e
Pedro Rodrigues <pedroxbrs@hotmail.com>
2024-09-02 11:10:21 -0300

    Fibo completo

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    Long lg1 = sc.nextLong();
+
+    System.out.println(Fibonacci.fibo(lg1));
+}
+
